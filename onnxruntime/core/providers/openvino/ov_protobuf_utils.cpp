223b5bcbc3df0019adec6ea94aaab1512686659c
Belem Zhang <belem.zhang@intel.com>
2026-07-28 17:25:39 +0800

    add onnxruntime/core/providers/openvino/ov_protobuf_utils.cpp (security finding)

--- onnxruntime/core/providers/openvino/ov_protobuf_utils.cpp
+++ onnxruntime/core/providers/openvino/ov_protobuf_utils.cpp
@@ -12,13 +12,14 @@ float get_float_initializer_data(const void* initializer) {
   const auto* tp = reinterpret_cast<const ONNX_NAMESPACE::TensorProto*>(initializer);
   ORT_ENFORCE((tp->has_data_type() && (tp->data_type() == ONNX_NAMESPACE::TensorProto_DataType_FLOAT)));
   // ORT_ENFORCE(initializer.dims_size() == 1);
+  ORT_ENFORCE(tp->float_data_size() >= 1, "FLOAT initializer has no float_data");
   return tp->float_data(0);
 }
 void set_float_initializer_data(const void* initializer, float data) {
   auto* tp = (ONNX_NAMESPACE::TensorProto*)(initializer);
   ORT_ENFORCE((tp->has_data_type() && (tp->data_type() == ONNX_NAMESPACE::TensorProto_DataType_FLOAT)));
   // ORT_ENFORCE(initializer.dims_size() == 1);
-  tp->set_float_data(0, data);
+  if (tp->float_data_size() < 1) tp->add_float_data(data); else tp->set_float_data(0, data);
 }
 }  // namespace openvino_ep
 }  // namespace onnxruntime
